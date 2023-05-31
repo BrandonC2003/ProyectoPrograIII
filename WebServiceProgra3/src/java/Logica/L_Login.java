7d5829639feb4de118ac8908c82d132a50393d87
CASTRO CONTRERAS BRANDON ALEXANDER <brandoncastro200314@gmail.com>
2023-05-31 10:15:16 -0600

    Cree el hash para encriptar datos

--- WebServiceProgra3/src/java/Logica/L_Login.java
+++ WebServiceProgra3/src/java/Logica/L_Login.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package Logica;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  *
@@ -11,4 +14,27 @@ package Logica;
  */
 public class L_Login {
     
+    public String SHA_256(String data){
+        try {
+            // Obtener instancia del algoritmo de hash SHA-256
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            
+            // Convertir los datos a bytes
+            byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
+            
+            // Aplicar el algoritmo de hash
+            byte[] hashedBytes = messageDigest.digest(bytes);
+            
+            // Convertir el resultado a una representación legible (hexadecimal)
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte b : hashedBytes) {
+                stringBuilder.append(String.format("%02x", b));
+            }
+            String hashedData = stringBuilder.toString();
+            
+            return hashedData;
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
 }
