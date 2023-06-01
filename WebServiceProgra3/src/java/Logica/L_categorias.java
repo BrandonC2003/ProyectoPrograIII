cc3fe7cd2ae661d1f90445f29c5717b7cb97b82a
Lmn1rix <ea521035@gmail.com>
2023-06-01 09:35:58 -0600

    método Listar y modificar Categoría

--- WebServiceProgra3/src/java/Logica/L_categorias.java
+++ WebServiceProgra3/src/java/Logica/L_categorias.java
@@ -6,8 +6,13 @@
 package Logica;
 
 import Modelos.Categorias;
+import Modelos.Ventas;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 
 
@@ -61,26 +66,33 @@ public class L_categorias {
             return false;
         }
     }
-    public boolean listarCategoria(Categorias objCategorias){
-        try (Connection conexion = Conexion.obtenerConexion();){
- 
-            String sqlCategoria = "SELECT * FROM categorias (Categoria,Descripcion) VALUES(?,?)";
+    public ArrayList<Categorias>listarCategoria() throws SQLException{
+        ArrayList<Categorias> lista = new ArrayList<Categorias>();
+        String consulta = "SELECT * From categorias;";
+        
+        try (Connection conexion = Conexion.obtenerConexion()){
+            Statement statement = conexion.createStatement();
+            ResultSet resultSet = statement.executeQuery(consulta);
             
-            CallableStatement categoria = conexion.prepareCall(sqlCategoria);
+            while (resultSet.next()) {
+                Categorias nuevaCategoria = new Categorias();
+                
+                nuevaCategoria.setIdCategoria(resultSet.getInt("IdCategoria"));
+                nuevaCategoria.setCategoria(resultSet.getString("Categoria"));
+                nuevaCategoria.setDescripcion(resultSet.getString("Descripcion"));
+                
+                lista.add(nuevaCategoria);
+            }
+            resultSet.close();
+            statement.close();
+  
+            return lista;
             
-            
-            categoria.setString(1, objCategorias.getCategoria());
-            categoria.setString(2, objCategorias.getDescripcion());
-            
-            categoria.execute();
-            
-            categoria.close();
-           
-            return true;
         } catch (Exception e) {
-            System.out.println("Error: al modificar datos. "+e.getMessage());
-            return false;
+            System.out.println("Error: "+e.getMessage());
+            lista = null;
         }
+        return lista;
     }
 }
      
