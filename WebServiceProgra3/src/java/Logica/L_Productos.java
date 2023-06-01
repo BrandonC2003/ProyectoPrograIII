864c475026365430a72087c41047cf639307d132
CASTRO CONTRERAS BRANDON ALEXANDER <brandoncastro200314@gmail.com>
2023-05-31 23:05:58 -0600

    cambios en categorias

---
+++ WebServiceProgra3/src/java/Logica/L_Productos.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+import Modelos.Productos;
+import java.util.ArrayList;
+/**
+ *
+ * @author brand
+ */
+public class L_Productos {
+    
+    public boolean insertarProductos(){
+        return true;
+    }
+    
+    public boolean modificarProductos(){
+        return true;
+    }
+    
+    public boolean agregarProductos(){
+        return true;
+    }
+    
+    public ArrayList<Productos> listarProductos(){
+        return null;
+    }
+}
