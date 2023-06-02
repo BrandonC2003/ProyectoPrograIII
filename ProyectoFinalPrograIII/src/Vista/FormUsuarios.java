d675627d78177b092564eca57a02fd34d8ddaf3c
CASTRO CONTRERAS BRANDON ALEXANDER <brandoncastro200314@gmail.com>
2023-06-01 22:49:48 -0600

    metodo de agregar usuarios

--- ProyectoFinalPrograIII/src/Vista/FormUsuarios.java
+++ ProyectoFinalPrograIII/src/Vista/FormUsuarios.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package Vista;
+import javax.swing.JOptionPane;
+import webservicebar.Usuarios;
+import webservicebar.WebServiceBar_Service;
+import webservicebar.WebServiceBar;
 
 /**
  *
@@ -14,10 +18,25 @@ public class FormUsuarios extends javax.swing.JPanel {
     /**
      * Creates new form FormUsuarios
      */
+    WebServiceBar_Service servicio;
+    WebServiceBar cliente;
     public FormUsuarios() {
         initComponents();
+        rbtVendedor.setSelected(true);
+        servicio = new WebServiceBar_Service();
+        cliente = servicio.getWebServiceBarPort();
     }
 
+    private String obtenerNivel(){
+        if(rbtAdministrador.isSelected()){
+            return "Administrador";
+        }
+        else if(rbtVendedor.isSelected()){
+            return "Vendedor";
+        }else{
+            return "Inventariador";
+        } 
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -29,6 +48,7 @@ public class FormUsuarios extends javax.swing.JPanel {
 
         buttonGroup1 = new javax.swing.ButtonGroup();
         buttonGroup2 = new javax.swing.ButtonGroup();
+        buttonGroup3 = new javax.swing.ButtonGroup();
         txfUsuario = new javax.swing.JTextField();
         lblUsuario = new javax.swing.JLabel();
         lblNombre = new javax.swing.JLabel();
@@ -36,11 +56,9 @@ public class FormUsuarios extends javax.swing.JPanel {
         lblClave = new javax.swing.JLabel();
         txfNombre = new javax.swing.JTextField();
         txfApellido = new javax.swing.JTextField();
-        txfClave = new javax.swing.JTextField();
         lblTelefono = new javax.swing.JLabel();
         txfTelefono = new javax.swing.JTextField();
         chbActivo = new javax.swing.JCheckBox();
-        chbDesactivado = new javax.swing.JCheckBox();
         rbtVendedor = new javax.swing.JRadioButton();
         rbtAdministrador = new javax.swing.JRadioButton();
         rbtInventariador = new javax.swing.JRadioButton();
@@ -48,6 +66,7 @@ public class FormUsuarios extends javax.swing.JPanel {
         btnEditar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblInformacion = new javax.swing.JTable();
+        passClave = new javax.swing.JPasswordField();
 
         setBackground(new java.awt.Color(0, 0, 0));
 
@@ -70,24 +89,28 @@ public class FormUsuarios extends javax.swing.JPanel {
         chbActivo.setForeground(new java.awt.Color(255, 255, 255));
         chbActivo.setText("Activo");
 
-        chbDesactivado.setBackground(new java.awt.Color(0, 0, 0));
-        chbDesactivado.setForeground(new java.awt.Color(255, 255, 255));
-        chbDesactivado.setText("Desactivado");
-
         rbtVendedor.setBackground(new java.awt.Color(0, 0, 0));
+        buttonGroup1.add(rbtVendedor);
         rbtVendedor.setForeground(new java.awt.Color(255, 255, 255));
         rbtVendedor.setText("Vendedor");
 
         rbtAdministrador.setBackground(new java.awt.Color(0, 0, 0));
+        buttonGroup1.add(rbtAdministrador);
         rbtAdministrador.setForeground(new java.awt.Color(255, 255, 255));
         rbtAdministrador.setText("Administrador");
 
         rbtInventariador.setBackground(new java.awt.Color(0, 0, 0));
+        buttonGroup1.add(rbtInventariador);
         rbtInventariador.setForeground(new java.awt.Color(255, 255, 255));
         rbtInventariador.setText("Inventariador");
 
         btnGuardar.setBackground(new java.awt.Color(102, 255, 255));
         btnGuardar.setText("Guardar");
+        btnGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuardarActionPerformed(evt);
+            }
+        });
 
         btnEditar.setBackground(new java.awt.Color(102, 255, 255));
         btnEditar.setText("Editar");
@@ -123,27 +146,21 @@ public class FormUsuarios extends javax.swing.JPanel {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(txfNombre)
                             .addComponent(txfApellido)
-                            .addComponent(txfClave)
                             .addComponent(txfTelefono)
-                            .addComponent(txfUsuario, javax.swing.GroupLayout.Alignment.TRAILING))
+                            .addComponent(txfUsuario, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(passClave))
                         .addGap(46, 46, 46))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
+                                .addGap(15, 15, 15)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addGap(15, 15, 15)
-                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                            .addComponent(rbtAdministrador)
-                                            .addComponent(rbtVendedor)
-                                            .addComponent(rbtInventariador)))
-                                    .addComponent(btnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(0, 0, Short.MAX_VALUE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(chbActivo)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 62, Short.MAX_VALUE)
-                                .addComponent(chbDesactivado)))
-                        .addGap(49, 49, 49)
+                                    .addComponent(rbtAdministrador)
+                                    .addComponent(rbtVendedor)
+                                    .addComponent(rbtInventariador)))
+                            .addComponent(btnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(chbActivo))
+                        .addGap(49, 145, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 406, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(btnEditar, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -167,15 +184,13 @@ public class FormUsuarios extends javax.swing.JPanel {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblClave)
-                    .addComponent(txfClave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(passClave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblTelefono)
                     .addComponent(txfTelefono, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(33, 33, 33)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(chbActivo)
-                    .addComponent(chbDesactivado))
+                .addComponent(chbActivo)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(28, 28, 28)
@@ -195,26 +210,47 @@ public class FormUsuarios extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
+        Usuarios usuario = new Usuarios();
+        
+        usuario.setNombreUsuario(txfUsuario.getText());
+        usuario.setNombre(txfNombre.getText());
+        usuario.setApellido(txfApellido.getText());
+        usuario.setClave(passClave.getPassword().toString());
+        usuario.setTelefono(txfTelefono.getText());
+        usuario.setEstado(chbActivo.isSelected() ? 1 : 0);
+        usuario.setNivel(obtenerNivel());
+        
+        boolean resultado = cliente.agregarUsuario(usuario.getNombreUsuario(), usuario.getNombre(), usuario.getApellido(), usuario.getClave(),usuario.getTelefono() , usuario.getEstado(), usuario.getNivel());
+        
+        if(resultado){
+            JOptionPane.showMessageDialog(this, "Usuario insertado exitosamente");
+        }else{
+            JOptionPane.showMessageDialog(this, "Ocurrio un error al intentar insertar el usuario");
+        }
+    }//GEN-LAST:event_btnGuardarActionPerformed
 
+
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnEditar;
     private javax.swing.JButton btnGuardar;
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.ButtonGroup buttonGroup2;
+    private javax.swing.ButtonGroup buttonGroup3;
     private javax.swing.JCheckBox chbActivo;
-    private javax.swing.JCheckBox chbDesactivado;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblApellido;
     private javax.swing.JLabel lblClave;
     private javax.swing.JLabel lblNombre;
     private javax.swing.JLabel lblTelefono;
     private javax.swing.JLabel lblUsuario;
+    private javax.swing.JPasswordField passClave;
     private javax.swing.JRadioButton rbtAdministrador;
     private javax.swing.JRadioButton rbtInventariador;
     private javax.swing.JRadioButton rbtVendedor;
     private javax.swing.JTable tblInformacion;
     private javax.swing.JTextField txfApellido;
-    private javax.swing.JTextField txfClave;
     private javax.swing.JTextField txfNombre;
     private javax.swing.JTextField txfTelefono;
     private javax.swing.JTextField txfUsuario;
